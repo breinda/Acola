99ce3a042090c3c23e1d607d23767e21a8140960
breinda <brendac@live.it>
2016-07-22 17:55:56 -0300

    Set initial view controller as GoalsVC if user has yet to choose a goal, and as currentStepVC otherwise

--- ChallengeHealth/AppDelegate.swift
+++ ChallengeHealth/AppDelegate.swift
@@ -8,6 +8,43 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         FIRApp.configure()
+        
+        var handle : FIRAuthStateDidChangeListenerHandle
+        
+        handle = (FIRAuth.auth()?.addAuthStateDidChangeListener { auth, user in
+            if let user = user {
+                // User is signed in.
+                let uid = user.uid;
+                
+                DAO.USERS_REF.child(uid).observeEventType(.ChildAdded, withBlock: { (snapshot) in
+                    
+                    // faz com a view inicial seja a de Goals, se o usuário não tiver escolhido nenhum goal, e que seja a de Steps, caso contrário
+                    
+                    if snapshot.key == "currentStepNumber" {
+                        
+                        let userStepNumber = snapshot.value as! String
+                        
+                        if userStepNumber == "0" {
+                            self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+                            let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                            let vc = mainStoryboard.instantiateViewControllerWithIdentifier("GoalsVC")
+                            self.window?.rootViewController = vc
+                        }
+                        else {
+                            self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+                            let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                            let vc = mainStoryboard.instantiateViewControllerWithIdentifier("currentStepVC")
+                            self.window?.rootViewController = vc
+                        }
+                        
+                        self.window?.makeKeyAndVisible()
+                    }
+                    
+                })
+            }
+            })!
+        
+        FIRAuth.auth()?.removeAuthStateDidChangeListener(handle)
         return true
     }
     
