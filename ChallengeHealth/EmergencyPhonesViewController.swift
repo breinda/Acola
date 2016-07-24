de2a94615a9310db0e58f33600532e8b99f982de
milasd <milasoaresdeoliveira@gmail.com>
2016-07-24 17:53:15 -0300

    Add Emergency Phones VC

---
+++ ChallengeHealth/EmergencyPhonesViewController.swift
@@ -0,0 +1,14 @@
+//
+//  EmergencyPhonesViewController.swift
+//  ChallengeHealth
+//
+//  Created by Mila Soares de Oliveira de Souza on 7/24/16.
+//  Copyright © 2016 Brenda Carrocino. All rights reserved.
+//
+
+import UIKit
+
+class EmergencyPhonesViewController: UIViewController {
+
+
+}
