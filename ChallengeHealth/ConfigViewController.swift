144f7c275e9eb4f8c5d5c0f4db485dfa15888d17
milasd <milasoaresdeoliveira@gmail.com>
2016-07-24 15:27:55 -0300

    Fixes ConfigVC buttons constraints

--- ChallengeHealth/ConfigViewController.swift
+++ ChallengeHealth/ConfigViewController.swift
@@ -4,16 +4,21 @@ import FirebaseAuth
 
 class ConfigViewController: UIViewController {
 
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var emailLabel: UILabel!
-    
+    @IBOutlet weak var boddiLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let user = FIRAuth.auth()?.currentUser {
             for profile in user.providerData {
+<<<<<<< cc2efa1e1cf17eaf3020052081851a67504a43a0
                 let email = profile.email
                 //nameLabel.text! = name!
+=======
+                //let email = profile.email
+                //let name = profile.displayName
+                //boddiLabel.text! = "Como posso lhe ajudar, \(name!)?"
+>>>>>>> Fixes ConfigVC buttons constraints
                 //emailLabel.text! = email!
             }
         }
