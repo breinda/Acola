07edb84ac35e4042ae4e133aa6337f3bd01b0283
Mila <milasoaresdeoliveira@gmail.com>
2016-07-28 03:48:13 -0300

    Add Emergency Phones VC animation

--- ChallengeHealth/EmergencyPhonesViewController.swift
+++ ChallengeHealth/EmergencyPhonesViewController.swift
@@ -2,8 +2,14 @@ import UIKit
 
 class EmergencyPhonesViewController: UIViewController {
 
+    @IBOutlet weak var boddiView: BoddiView!
+    
     @IBAction func returnButtonWasTapped(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        boddiView.addNormalCycleAnimation()
     }
 
 }
