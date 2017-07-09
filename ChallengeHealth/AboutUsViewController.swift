f0ea664bd53210f8fabe9d86cd767c39ce0ba990
Brenda Carrocino <breinda@xumimux.local>
2017-07-09 19:11:09 -0300

    Implement working layout on SummitReachedVC; Set initial scroll to 0 on AboutUsVC

--- ChallengeHealth/AboutUsViewController.swift
+++ ChallengeHealth/AboutUsViewController.swift
@@ -2,7 +2,16 @@ import UIKit
 
 class AboutUsViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var aboutTextView: UITextView!
+    
     override func viewDidLoad() {
+        
+    }
+    
+    // faz com que a textView apareça scrollada a partir do início
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        aboutTextView.setContentOffset(CGPoint.zero, animated: false)
     }
     
     @IBAction func returnButtonWasTapped(_ sender: AnyObject) {
