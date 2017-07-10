32ce070cbf9b414f9f75ef3750500b5ef4507dae
Brenda <brendac@live.it>
2017-07-10 03:15:05 -0300

    Implement email sending feature at AboutUsVC

--- ChallengeHealth/AboutUsViewController.swift
+++ ChallengeHealth/AboutUsViewController.swift
@@ -1,11 +1,11 @@
 import UIKit
+import MessageUI
 
-class AboutUsViewController: ElasticModalViewController/*UIViewController*/, UIScrollViewDelegate {
+class AboutUsViewController: ElasticModalViewController/*UIViewController*/, UIScrollViewDelegate, MFMailComposeViewControllerDelegate {
 
     @IBOutlet weak var aboutTextView: UITextView!
     
     override func viewDidLoad() {
-        
     }
     
     // faz com que a textView apareça scrollada a partir do início
@@ -14,8 +14,46 @@ class AboutUsViewController: ElasticModalViewController/*UIViewController*/, UIS
         aboutTextView.setContentOffset(CGPoint.zero, animated: false)
     }
     
+    
+    // MARK: Navigation
+    
     @IBAction func returnButtonWasTapped(_ sender: AnyObject) {
         self.modalTransition.edge = .right
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    // MARK: Email handling
+    
+    @IBAction func sendEmail(_ sender: Any) {
+        
+        let mc: MFMailComposeViewController = MFMailComposeViewController()
+        mc.mailComposeDelegate = self
+        
+        mc.setSubject("[ACOLÁ] Olá!")
+        
+        let mensagemPadrao = "(bata um papo conosco! blablablá.......)"
+        
+        mc.setMessageBody(mensagemPadrao, isHTML: false)
+        mc.setToRecipients(["brendac@live.it"])
+        
+        self.present(mc, animated: true, completion: nil)
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        
+        switch result.rawValue {
+        case MFMailComposeResult.cancelled.rawValue:
+            NSLog("Email Cancelado")
+        case MFMailComposeResult.saved.rawValue:
+            NSLog("Email Salvo")
+        case MFMailComposeResult.sent.rawValue:
+            NSLog("Email Enviado")
+        case MFMailComposeResult.failed.rawValue:
+            NSLog("Falha no Envio do Email")
+        default: break
+        }
+        
         self.dismiss(animated: true, completion: nil)
     }
     
