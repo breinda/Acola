c7bb44ee993ff097fad0a9866ad4dd73b17664e2
Brenda Carrocino <breinda@xumimux.local>
2017-07-24 21:31:42 -0300

    Finish polishing up layout at NewGoalVC

--- ChallengeHealth/NewGoalViewController.swift
+++ ChallengeHealth/NewGoalViewController.swift
@@ -8,12 +8,30 @@
 
 import UIKit
 
-class NewGoalViewController: ElasticModalViewController {
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+class NewGoalViewController: ElasticModalViewController, UITextViewDelegate {
 
     @IBOutlet weak var backRectangleImageView: UIImageView!
     @IBOutlet weak var bubbleImageView: UIImageView!
     @IBOutlet weak var boddiImageView: UIImageView!
     @IBOutlet weak var navBarRectangleImageView: UIImageView!
+    @IBOutlet weak var goalRectangleImageView: UIImageView!
+    
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var boddiTextView: UITextView!
+    
+    var placeholder : String = "seu objetivo!"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +43,9 @@ class NewGoalViewController: ElasticModalViewController {
         
         navBarRectangleImageView.layer.borderWidth = 1
         navBarRectangleImageView.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.12).cgColor
+        
+        goalRectangleImageView.layer.borderWidth = 1
+        goalRectangleImageView.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.1).cgColor
         
         bubbleImageView.layer.cornerRadius = 26
         bubbleImageView.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
@@ -38,7 +59,58 @@ class NewGoalViewController: ElasticModalViewController {
         boddiImageView.layer.shadowRadius = 4
         boddiImageView.layer.shadowOffset = CGSize(width: 0, height: 2)
         boddiImageView.layer.shouldRasterize = true
+        
+        // setando propriedades da goalTextView
+        goalTextView.text = placeholder
+        goalTextView.keyboardAppearance = .dark
+        goalTextView.delegate = self
+        self.hideKeyboardWhenTappedAround()
+
+        boddiTextView.delegate = self
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        goalTextView.addObserver(self, forKeyPath: "contentSize", options: NSKeyValueObservingOptions.new, context: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        goalTextView.removeObserver(self, forKeyPath: "contentSize")
+    }
+    
+    
+    // MARK: Text View Properties
+    
+    // force the text in a UITextView to always center itself.
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        let textView = object as! UITextView
+        var topCorrect = (textView.bounds.size.height - textView.contentSize.height * textView.zoomScale) / 2
+        topCorrect = topCorrect < 0.0 ? 0.0 : topCorrect;
+        textView.contentInset.top = topCorrect
+    }
+    
+    // faz com que a textView apareça scrollada a partir do início
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        boddiTextView.setContentOffset(CGPoint.zero, animated: false)
+        //goalTextView.setContentOffset(CGPoint.zero, animated: false)
+    }
+
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.text == placeholder {
+            textView.text = ""
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text == "" {
+            textView.text = placeholder
+        }
+    }
+    
+    
+    // MARK: Navigation
     
     @IBAction func backButtonWasTapped(_ sender: AnyObject) {
         
