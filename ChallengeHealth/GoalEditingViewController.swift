3644d85a37f468bf66571e0bc017f65a5d266086
Brenda Carrocino <breinda@xumimux.local>
2017-07-26 03:51:50 -0300

    Create GoalEditingVC; Add a Save icon to assets; Put constraints on existing layout

---
+++ ChallengeHealth/GoalEditingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GoalEditingViewController.swift
+//  ChallengeHealth
+//
+//  Created by Brenda Carrocino on 26/07/17.
+//  Copyright © 2017 Brenda Carrocino. All rights reserved.
+//
+
+import UIKit
+
+class GoalEditingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
