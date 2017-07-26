d447a42e4df83b32e8613fb00e7860fbc54e16ab
Brenda <brendac@live.it>
2017-07-26 14:14:55 -0300

    Create segue from NewGoalVC to GoalEditingVC; Fix existing layout on GoalEditingVC

--- ChallengeHealth/GoalEditingViewController.swift
+++ ChallengeHealth/GoalEditingViewController.swift
@@ -9,27 +9,40 @@
 import UIKit
 
 class GoalEditingViewController: UIViewController {
+    
+    @IBOutlet weak var bgRectangleImageView: UIImageView!
+    @IBOutlet weak var backRectangleImageView: UIImageView!
+    @IBOutlet weak var bubbleImageView: UIImageView!
+    @IBOutlet weak var boddiImageView: UIImageView!
+    @IBOutlet weak var navBarRectangleImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // setando propriedades das imagens
+        bgRectangleImageView.layer.borderWidth = 1
+        bgRectangleImageView.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.1).cgColor
+        
+        backRectangleImageView.layer.cornerRadius = 39
+        backRectangleImageView.layer.borderWidth = 1
+        backRectangleImageView.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        
+        navBarRectangleImageView.layer.borderWidth = 1
+        navBarRectangleImageView.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.12).cgColor
+        
+        bubbleImageView.layer.cornerRadius = 26
+        bubbleImageView.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
+        bubbleImageView.layer.shadowOpacity = 0.1
+        bubbleImageView.layer.shadowRadius = 4
+        bubbleImageView.layer.shadowOffset = CGSize(width: 0, height: 2)
+        bubbleImageView.layer.shouldRasterize = true
+        
+        boddiImageView.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
+        boddiImageView.layer.shadowOpacity = 0.1
+        boddiImageView.layer.shadowRadius = 4
+        boddiImageView.layer.shadowOffset = CGSize(width: 0, height: 2)
+        boddiImageView.layer.shouldRasterize = true
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
