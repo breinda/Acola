ed4d117a0e90b9e3166fdab182fbb658c1a56d3e
Brenda Carrocino <breinda@xumimux.local>
2017-08-01 12:42:26 -0300

    Update layout on EmergencyPhonesVC

--- ChallengeHealth/EmergencyPhonesViewController.swift
+++ ChallengeHealth/EmergencyPhonesViewController.swift
@@ -31,6 +31,13 @@ class EmergencyPhonesViewController: ElasticModalViewController/*UIViewControlle
     
     @IBOutlet weak var phonesCollectionView: UICollectionView!
     
+    //@IBOutlet weak var boddiLabel: UITextView!
+    
+    @IBOutlet weak var backRectangleImageView: UIImageView!
+    @IBOutlet weak var bubbleImageView: UIImageView!
+    @IBOutlet weak var boddiImageView: UIImageView!
+    @IBOutlet weak var navBarRectangleImageView: UIImageView!
+    
     let defaultName: String = "CENTRO DE VALORIZAÇÃO DA VIDA"
     let defaultDesc: String = "Apoio emocional 24 horas"
     let defaultNum: String = "disque 141"
@@ -38,6 +45,28 @@ class EmergencyPhonesViewController: ElasticModalViewController/*UIViewControlle
     
     override func viewDidLoad() {
       //  boddi.addNormalCycleAnimation()
+        
+        backRectangleImageView.layer.cornerRadius = 39
+        backRectangleImageView.layer.borderWidth = 1
+        backRectangleImageView.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        
+        navBarRectangleImageView.layer.borderWidth = 1
+        navBarRectangleImageView.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.12).cgColor
+        
+        bubbleImageView.layer.cornerRadius = 26
+        bubbleImageView.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
+        bubbleImageView.layer.shadowOpacity = 0.1
+        bubbleImageView.layer.shadowRadius = 4
+        bubbleImageView.layer.shadowOffset = CGSize(width: 0, height: 2)
+        bubbleImageView.layer.shouldRasterize = true
+        
+        boddiImageView.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
+        boddiImageView.layer.shadowOpacity = 0.1
+        boddiImageView.layer.shadowRadius = 4
+        boddiImageView.layer.shadowOffset = CGSize(width: 0, height: 2)
+        boddiImageView.layer.shouldRasterize = true
+        
+        //boddiLabel.delegate = self
     }
     
     
